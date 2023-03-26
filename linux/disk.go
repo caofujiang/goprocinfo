2731a7b4c05de944a0ff198a10eddd86993f4307
admin <admin@192.168.2.3>
2023-03-26 11:53:04 +0800

    disk fix

--- linux/disk.go
+++ linux/disk.go
@@ -1,10 +1,5 @@
 package linux
 
-import (
-	"runtime"
-	"syscall"
-)
-
 type Disk struct {
 	All        uint64 `json:"all"`
 	Used       uint64 `json:"used"`
@@ -14,16 +9,16 @@ type Disk struct {
 
 func ReadDisk(path string) (*Disk, error) {
 	disk := Disk{}
-	if runtime.GOOS != "windows" {
-		fs := syscall.Statfs_t{}
-		err := syscall.Statfs(path, &fs)
-		if err != nil {
-			return nil, err
-		}
-		disk.All = fs.Blocks * uint64(fs.Bsize)
-		disk.Free = fs.Bfree * uint64(fs.Bsize)
-		disk.Used = disk.All - disk.Free
-		disk.FreeInodes = fs.Ffree
-	}
+	//if runtime.GOOS != "windows" {
+	//	fs := syscall.Statfs_t{}
+	//	err := syscall.Statfs(path, &fs)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	disk.All = fs.Blocks * uint64(fs.Bsize)
+	//	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	//	disk.Used = disk.All - disk.Free
+	//	disk.FreeInodes = fs.Ffree
+	//}
 	return &disk, nil
 }
